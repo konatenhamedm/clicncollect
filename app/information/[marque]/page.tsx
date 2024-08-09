f3b5a80f6f0e1eb81b3b2bcbdc719c004b664d09
konaten <konatenhamed@gmail.com>
2024-08-09 10:08:54 +0000

    sagddjfff

--- app/information/[marque]/page.tsx
+++ app/information/[marque]/page.tsx
@@ -1,9 +1,10 @@
+"use client";
 import FormStep from "@/components/formulaire/formStep";
-//import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 function Page({ params }: { params: { marque: string } }) {
-  //const router = useRouter();
+  const router = useRouter();
   return (
     <div className=" w-full flex  py-7 mt-[100px]  bg-[#F5F5F5] items-center justify-center pb-[10rem]">
       <FormStep />
